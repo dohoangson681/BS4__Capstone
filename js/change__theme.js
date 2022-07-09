eb6c964089d727cb93e0e84bd729d6e23c5e4b5d
Son <dohoangson060801@gmail.com>
2022-07-09 08:53:33 +0700

    Res SE

--- js/change__theme.js
+++ js/change__theme.js
@@ -1,3 +1,6 @@
 document.getElementById("theme__button").onclick = function () {
     document.getElementById("body").classList.toggle("dark") ; 
 }
+document.getElementById("theme__button2").onclick= function (){
+    document.getElementById("body").classList.toggle("dark") ;
+}
